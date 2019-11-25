c225bc2fbb88f7480300ca631092045c3a76f729
pcysl5edgo <pcysl5edgo@gmail.com>
2019-11-25 18:25:36 +0900

    a

--- dist/index.js
+++ dist/index.js
@@ -149,25 +149,31 @@ function ExecuteSetUp(download_url) {
                     return [4 /*yield*/, exec.exec('UnitySetup64.exe /S /D="C:\Program Files\Unity"')];
                 case 3:
                     _b.sent();
-                    return [3 /*break*/, 11];
+                    return [3 /*break*/, 13];
                 case 4: return [4 /*yield*/, exec.exec('curl -OL ' + download_url)];
                 case 5:
                     _b.sent();
                     return [4 /*yield*/, exec.exec("sudo installer -package Unity.pkg -target /")];
                 case 6:
                     _b.sent();
-                    return [3 /*break*/, 11];
-                case 7: return [4 /*yield*/, exec.exec('wget ' + download_url + ' -O UnitySetUp')];
+                    return [3 /*break*/, 13];
+                case 7: return [4 /*yield*/, exec.exec('sudo apt-get update')];
                 case 8:
                     _b.sent();
-                    return [4 /*yield*/, exec.exec('sudo chmod +x UnitySetUp')];
+                    return [4 /*yield*/, exec.exec('sudo apt-get -y install libgtk-3-dev libglu1-mesa libxi-dev libxmu-dev libglu1-mesa-dev libnss3-dev libsound2-dev libgconf2-dev')];
                 case 9:
                     _b.sent();
-                    return [4 /*yield*/, exec.exec('echo y | sudo UnitySetUp --unattended --install-location=/opt/Unity --verbose --download-location=/tmp/unity --components=Unity')];
+                    return [4 /*yield*/, exec.exec('wget ' + download_url + ' -O UnitySetUp')];
                 case 10:
                     _b.sent();
-                    return [3 /*break*/, 11];
-                case 11: return [2 /*return*/];
+                    return [4 /*yield*/, exec.exec('sudo chmod +x UnitySetUp')];
+                case 11:
+                    _b.sent();
+                    return [4 /*yield*/, exec.exec('echo y | sudo UnitySetUp --unattended --install-location=/opt/Unity --verbose --download-location=/tmp/unity --components=Unity')];
+                case 12:
+                    _b.sent();
+                    return [3 /*break*/, 13];
+                case 13: return [2 /*return*/];
             }
         });
     });
